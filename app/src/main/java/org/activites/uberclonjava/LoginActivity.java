7f7e4a9dfcc4d3a0d3db8613b01e583c6a380fd0
Oscar <oscarsullivanscorpion@gmail.com>
2020-06-08 13:20:22 -0500

    Login Activity created

---
+++ app/src/main/java/org/activites/uberclonjava/LoginActivity.java
@@ -0,0 +1,14 @@
+package org.activites.uberclonjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/org/activites/uberclonjava/SelectActivity.java
+++ app/src/main/java/org/activites/uberclonjava/SelectActivity.java
@@ -1,14 +1,22 @@
 package org.activites.uberclonjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectActivity extends AppCompatActivity {
 
+    Toolbar mToolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+        mToolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("Seleccione una opción");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
