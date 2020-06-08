19dbad7e62421c121fb582b3d5a37821fd4b56c1
Oscar Regis <oregis@anzen.com.mx>
2020-06-08 13:22:47 -0500

    fix

--- app/src/main/java/org/activites/uberclonjava/RegisterActivity.java
+++ app/src/main/java/org/activites/uberclonjava/RegisterActivity.java
@@ -2,13 +2,27 @@ package org.activites.uberclonjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Button mButtonGoToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(RegisterActivity.this, LoginAc);
     }
 }
